ed5459bbfc7f269da8ef1171e69beec92a08c4bd
araxabd <arianabdollahian@gmail.com>
2026-07-04 17:12:06 +0330

    start courses app

---
+++ project/courses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+class Course(models.Model):
+    title = models.CharField(max_length=150)
+    desc = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses')
+    #TODO: add marketplace features
+
+    def __str__(self):
+        return self.title
