5676cff4e8836d9aba3ee9ab0470962cf88e0742
araxabd <arianabdollahian@gmail.com>
2026-07-04 18:18:41 +0330

    basic logout

--- project/users/views.py
+++ project/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('')
