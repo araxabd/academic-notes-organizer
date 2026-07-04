8e7f017e40c62d3998ba132fafa261d1d11be2af
araxabd <arianabdollahian@gmail.com>
2026-07-04 17:57:30 +0330

    show basic course list

--- project/courses/views.py
+++ project/courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Course
 
-# Create your views here.
+@login_required
+def course_list(request):
+    courses = Course.objects.filter(owner=request.user)
+    context = {
+            'courses': courses
+            }
+    return render(request, 'courses/course_list.html', context)
