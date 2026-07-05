da3358ede3ff63b900ff14f54719dfb410a74245
araxabd <arianabdollahian@gmail.com>
2026-07-05 19:05:41 +0330

    basic note create

--- project/notes/urls.py
+++ project/notes/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'notes'
 
 urlpatterns = [
-        path('<int:note_id>/', views.note_detail, name="detail")
+        path('<int:note_id>/', views.note_detail, name="detail"),
+        path('course_<int:course_id>/create', views.note_create, name="create")
 ]
--- project/notes/views.py
+++ project/notes/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from courses.models import Course
 from .models import Note
+from .forms import NoteForm
 
 @login_required
 def note_detail(request, note_id):
     note = get_object_or_404(Note, id=note_id, owner=request.user)
     return render(request, 'notes/note_detail.html', {'note': note})
+
+@login_required
+def note_create(request, course_id):
+    course = get_object_or_404(Course, id=course_id, owner=request.user)
+
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.owner = request.user
+            note.course = course
+            note.save()
+            return redirect("notes:detail", note_id=note.id)
+    else:
+        form = NoteForm()
+    return render(request, 'notes/note_create.html', {'form': form})
