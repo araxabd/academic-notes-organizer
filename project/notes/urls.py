15069a129e64043eb0721322fe209562ac563733
araxabd <arianabdollahian@gmail.com>
2026-07-05 18:14:36 +0330

    basic note detail

---
+++ project/notes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'notes'
+
+urlpatterns = [
+        path('<int:note_id>/', views.note_detail, name="detail")
+]
--- project/notes/views.py
+++ project/notes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Note
 
-# Create your views here.
+@login_required
+def note_detail(request, note_id):
+    note = get_object_or_404(Note, id=note_id, owner=request.user)
+    return render(request, 'notes/note_detail.html', {'note': note})
