6eba69a49c8541c701c2a8149293a0b3d8d04d88
araxabd <arianabdollahian@gmail.com>
2026-07-05 14:26:27 +0330

    user basic signup

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("courses:list")
+    else:
+        form = SignUpForm()
+    return render(request, 'users/user_signup.html', { 'form': form })
 
 @login_required
 def user_logout(request):
