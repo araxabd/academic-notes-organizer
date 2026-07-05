7f2bef8dab1b4973585f5cda4380c46f1b91022f
araxabd <arianabdollahian@gmail.com>
2026-07-05 14:26:02 +0330

    name urls

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "users"
+
 urlpatterns = [
-        path('logout/', views.user_logout)
+        path('logout/', views.user_logout, name="logout"),
+        path('signup/', views.user_signup, name="signup")
 ]
