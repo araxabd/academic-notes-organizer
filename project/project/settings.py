92f6335fdc5e627455b2dbb96f1801e606b3d122
araxabd <arianabdollahian@gmail.com>
2026-07-16 13:32:37 +0330

    start marketplace app

--- project/project/settings.py
+++ project/project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'courses.apps.CoursesConfig',
-    'notes.apps.NotesConfig'
+    'notes.apps.NotesConfig',
+    'marketplace.apps.MarketplaceConfig'
 ]
 
 MIDDLEWARE = [
