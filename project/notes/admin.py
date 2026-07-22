abc04216c65e8848cb7840bc853e36b04d434a7f
araxabd <arianabdollahian@gmail.com>
2026-07-22 18:51:53 +0330

    register NoteFile and Tag to admin

--- project/notes/admin.py
+++ project/notes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Note
+from .models import Note, NoteFile, Tag
 
 admin.site.register(Note)
+admin.site.register(NoteFile)
+admin.site.register(Tag)
