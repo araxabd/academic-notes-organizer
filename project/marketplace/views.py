f6f25d6bcf260454cf36d13c335a730ad808fbb7
araxabd <arianabdollahian@gmail.com>
2026-07-24 16:39:13 +0330

    search and filters for marketplace courselist

--- project/marketplace/views.py
+++ project/marketplace/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Avg
+from django.db.models import Avg, Q
 from django.core.paginator import Paginator
 from courses.models import Course
 from .models import Rating, Comment
 from .forms import RatingForm, CommentForm
 
 def course_public_list(request):
-    courses = Course.objects.filter(is_public=True).annotate(rating=Avg("ratings__score")).order_by("-created")
+    courses = Course.objects.filter(is_public=True).annotate(rating=Avg("ratings__score"))
+    q = request.GET.get("q", "").strip()
+    from_date = request.GET.get("from")
+    to_date = request.GET.get("to")
+    min_price = request.GET.get("min")
+    max_price = request.GET.get("max")
+    order = request.GET.get("order")
+    if q:
+        courses = courses.filter(Q(title__icontains=q) | Q(desc__icontains=q) | Q(owner__username__icontains=q))
+
+    if from_date:
+        courses = courses.filter(created__date__gte=from_date)
+
+    if to_date:
+        courses = courses.filter(created__date__lte=to_date)
+
+    if min_price:
+        courses = courses.filter(price__gte=min_price)
+
+    if max_price:
+        courses = courses.filter(price__lte=max_price)
+
+    if order == "rating":
+        courses = courses.order_by("-rating")
+    elif order == "cheap":
+        courses = courses.order_by("price")
+    elif order == "expensive":
+        courses = courses.order_by("-price")
+    elif order == "old":
+        courses = courses.order_by("created")
+    else:
+        courses = courses.order_by("-created")
+
     paginator = Paginator(courses, 5)
     page_number = request.GET.get('page')
     page_courses = paginator.get_page(page_number)
