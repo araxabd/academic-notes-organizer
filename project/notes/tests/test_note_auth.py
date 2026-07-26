1ddad087ac5f5d95ffbee8d3abd5a22709e7f1ae
araxabd <arianabdollahian@gmail.com>
2026-07-26 16:57:19 +0330

    better test naming

--- project/notes/tests/test_note_auth.py
+++ project/notes/tests/test_note_auth.py
@@ -12,22 +12,22 @@ class CourseAuthTest(TestCase):
         self.course = Course.objects.create(title="Test Course", desc="This is a TestCourse", owner=self.user)
         self.note = Note.objects.create(title="Test Note", desc="This is a TestNote", content="content", course=self.course, owner=self.user)
     
-    def test_course_list_by_anonymous(self):
+    def test_note_list_by_anonymous(self):
         response = self.client.get(reverse("courses:detail", args=[self.course.id]))
         self.assertRedirects(response, f"{reverse('users:login')}?next={reverse('courses:detail', args=[self.course.id])}")
 
-    def test_course_detail_by_anonymous(self):
+    def test_note_detail_by_anonymous(self):
         response = self.client.get(reverse("notes:detail", args=[self.note.id]))
         self.assertRedirects(response, f"{reverse('users:login')}?next={reverse('notes:detail', args=[self.note.id])}")
 
-    def test_course_create_by_anonymous(self):
+    def test_note_create_by_anonymous(self):
         response = self.client.get(reverse("notes:create", args=[self.course.id]))
         self.assertRedirects(response, f"{reverse('users:login')}?next={reverse('notes:create', args=[self.course.id])}")
 
-    def test_course_update_by_anonymous(self):
+    def test_note_update_by_anonymous(self):
         response = self.client.get(reverse("notes:update", args=[self.note.id]))
         self.assertRedirects(response, f"{reverse('users:login')}?next={reverse('notes:update', args=[self.note.id])}")
 
-    def test_course_delete_by_anonymous(self):
+    def test_note_delete_by_anonymous(self):
         response = self.client.get(reverse("notes:delete", args=[self.note.id]))
         self.assertRedirects(response, f"{reverse('users:login')}?next={reverse('notes:delete', args=[self.note.id])}")
